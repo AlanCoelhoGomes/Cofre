936bf01da6df85afb7f868e925e162167aa268ed
Alan <138327005+Dadinho68@users.noreply.github.com>
2024-04-23 20:51:13 -0300

    Add files via upload

---
+++ Cofre/uninter/src/Uninter/Euro.java
@@ -0,0 +1,19 @@
+package Uninter;
+
+public class Euro extends Moeda {
+
+    public Euro(double valorInicial){
+        this.valor = valorInicial;
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Euro - " + valor);
+    }
+
+    @Override
+    public double converter() {
+        return this.valor * 4.5;
+    }
+
+}
