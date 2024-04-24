c1e5d085d530b8f7d74f46e336ecf32a6f6930bf
Alan <138327005+Dadinho68@users.noreply.github.com>
2024-04-24 11:30:17 -0300

    Update Real.java

--- Cofre/uninter/src/Uninter/Real.java
+++ Cofre/uninter/src/Uninter/Real.java
@@ -14,6 +14,21 @@ public class Real extends Moeda {
     @Override
     public double converter() {
         return this.valor;
+    }
+        
+    @Override
+    public boolean equals(Object objeto) {
+        if (this.getClass() != objeto.getClass()) {
+            return false;
         }
-
+    
+        Real objetoDeReal = (Real) objeto;
+    
+        if (this.valor != objetoDeReal.valor){
+            return false;
+        }
+    
+        return true;
+    }
+        
 }
